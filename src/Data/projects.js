8be6eeb7cec45b81cac240c17eee163771d72182
TegSingh <tegveer2211@gmail.com>
2021-12-28 20:28:49 -0500

    Updated JSON files

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -22,7 +22,7 @@ const projects = [
             "The image shows a simulation with clusters formed around the moving objects indicating that they have been succesfully detected",
             "A research paper for this was approved and published"
         ],
-        "source": "",
+        "source": "https://ieeexplore.ieee.org/abstract/document/9589778",
         "image": motl
     },
     {
@@ -44,21 +44,85 @@ const projects = [
     },
     {
         "id": 3,
+        "title": "Automated Grading Framwork",
+        "category": "Coursework",
+        "technologies": [
+            "Java", "CSV", "Streams", "RMI", "TCP Socket API", "OOP"
+        ],
+        "completion": "2021-12-01",
+        "description": [
+            "Created a distributed system using Java to automatically grade student assignments", 
+            "Used Java’s Socket API to exchange messages and files between clients and server",
+            "Used Dynamic Data Processing for MCQ and Short Answer Grading", 
+        ],
+        "source": "https://github.com/TegSingh/Automated-Grading-Framework",
+        "image": tsch
+    },
+    {
+        "id": 4,
+        "title": "Population Tracking System",
+        "category": "Coursework",
+        "technologies": [
+            "Django", "REST", "API", "JavaScript", "PHP", "Arduino", "MQTT"
+        ],
+        "completion": "2021-12-03",
+        "description": [
+            "The app has been developed using Django and uses MQTT for pub-sub communication",
+            "The application was hosted and managed on Cloud and remotely managed through Putty",
+            "The system tracks the number of people in indoor facilities using ESP8266 and a laser sensor"
+        ],
+        "source": "https://github.com/TegSingh/IoT-Project-Population-Tracker",
+        "image": tsch
+    },
+    {
+        "id": 5,
+        "title": "iMarker-Student-Edition",
+        "category": "Coursework",
+        "technologies": [
+            "Java", "Android Studio", "SQLite"
+        ],
+        "completion": "2021-12-06",
+        "description": [
+            "THis is a mobile application developed using android studio and java", 
+            "The application allows the student to sell and buy products based on their location and age", 
+            "Location services and CRUD using SQLite was added to app to filter products and users"
+        ],
+        "source": "https://github.com/TegSingh/iMarket-Student-Edition",
+        "image": tsch
+    },
+    {
+        "id": 6,
         "title": "Embedded Linux Vulnerability DS",
         "category": "Research",
         "technologies": [
             "C/C++"
         ],
-        "completion": "currently in progress",
+        "completion": "Complete",
         "description": [
             "This project aims to create a dataset to train a software to flag vulnerabilities in Embedded Softwares",
-            "The dataset contains code snippets labelled as being severe or non severe Linux source code security vulnerabilities"
+            "The dataset contains code snippets labelled as being severe or non severe Linux source code security vulnerabilities", 
+            "Contributed as an author to a research paper - not yet published"
         ],
         "source": "https://github.com/TegSingh/Embedded-Software-Dataset",
         "image": vulnerabilitydataset
     },
     {
-        "id": 4,
+        "id": 7,
+        "title": "Github Stats - Python",
+        "category": "Research",
+        "technologies": [
+            "Python", "Git", "GitPython", "data structures"
+        ],
+        "completion": "Currently in progress",
+        "description": [
+            "This project contains scripts to get github author, commit and file stats",
+            "The scripts generates stats like #commits/authors per file, #commits/author, along with other commit details",
+        ],
+        "source": "https://github.com/TegSingh/Github-Python",
+        "image": gitpy
+    },
+    {
+        "id": 8,
         "title": "Navigation System",
         "category": "Course",
         "technologies": [
@@ -74,22 +138,7 @@ const projects = [
         "image": navigation
     },
     {
-        "id": 5,
-        "title": "Github Stats - Python",
-        "category": "Research",
-        "technologies": [
-            "Python", "Git", "GitPython", "data structures"
-        ],
-        "completion": "Currently in progress",
-        "description": [
-            "This project contains scripts to get github author, commit and file stats",
-            "The scripts generates stats like #commits/authors per file, #commits/author, along with other commit details",
-        ],
-        "source": "https://github.com/TegSingh/Github-Python",
-        "image": gitpy
-    },
-    {
-        "id": 6,
+        "id": 9,
         "title": "Student Assistant App",
         "category": "Personal",
         "technologies": [
@@ -97,14 +146,14 @@ const projects = [
         ],
         "completion": "Currently in progress",
         "description": [
-            "This app uses MERNG stack to create an app to assist student schedule",
-            "It currently contains features like GPA and course grade Calculator",
+            "This app uses Spring boot stack to create an app to assist students in managing their schedule schedule",
+            "It currently contains features like GPA and course grade Calculator and a Todo/Notes list",
         ],
         "source": "",
         "image": merng
     },
     {
-        "id": 7,
+        "id": 10,
         "title": "Social Networking Site",
         "category": "Personal",
         "technologies": [
